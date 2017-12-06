ed42fd562b106d389a354aeaaf50d00325ae861b
seahailang <seahailang@gmail.com>
2017-12-06 16:42:54 +0800

    config file for some model

---
+++ config.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: 0.0
+@author: hailang
+@Email: seahailang@gmail.com
+@software: PyCharm Community Edition
+@file: config.py
+@time: 2017/12/4 19:11
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+
+
+FLAGS = tf.app.flags.FLAGS
+
+tf.app.flags.DEFINE_integer('batch_size','256','')
+tf.app.flags.DEFINE_integer('latent_dims',100,'')
+tf.app.flags.DEFINE_float('learning_rate',0.00001,'')
+tf.app.flags.DEFINE_integer('channel',1,
+                            '''image channel''')
+tf.app.flags.DEFINE_integer('image_w',28,
+                            '''image width''')
+tf.app.flags.DEFINE_integer('image_h',28,
+                            '''image height''')
+tf.app.flags.DEFINE_integer('decay_steps',10000,'')
+tf.app.flags.DEFINE_float('decay_rate',0.8,'')
+tf.app.flags.DEFINE_string('ckpt','./ckpt0/','')
+tf.app.flags.DEFINE_integer('max_steps','1000000','')
+
+if __name__ == '__main__':
+    pass
